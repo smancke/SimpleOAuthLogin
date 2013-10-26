c06f7667980a4c6fb6a7a79567ac7b3bec5f3e8a
Sebastian Mancke <s.mancke@tarent.de>
2013-10-26 23:32:02 +0200

    implemented google+ person fetching

--- SimpleGoogleLogin.php
+++ SimpleGoogleLogin.php
@@ -7,7 +7,9 @@ class SimpleGoogleLogin {
     private $URLS = array(
                           'token' => 'https://accounts.google.com/o/oauth2/token',
                           'auth' => 'https://accounts.google.com/o/oauth2/auth',
-                          'tokenInfo' => 'https://www.googleapis.com/oauth2/v1/tokeninfo'
+                          'tokenInfo' => 'https://www.googleapis.com/oauth2/v1/tokeninfo',
+                          'mePeople' => 'https://www.googleapis.com/plus/v1/people/me/people/visible',
+                          'me' => 'https://www.googleapis.com/plus/v1/people/me'
                           );
 
     private $config;
@@ -37,7 +39,7 @@ class SimpleGoogleLogin {
      * Returns the login url for google.
      * (The url to forward to)
      */
-    public function getAuthUrl() {
+    public function getAuthUrl($plusScope=false) {
         $authParams = ['response_type' => 'code',
                        'redirect_uri' => $this->config['redirect_uri'],
                        'client_id' => $this->config['client_id'],
@@ -45,6 +47,8 @@ class SimpleGoogleLogin {
                        'access_type' => 'offline',
                        'approval_prompt' => 'force'];
 
+        if ($plusScope)
+            $authParams['scope'] .= ' https://www.googleapis.com/auth/plus.login';
         return $this->URLS['auth'] .'?'. $this->http->arrayFormEncode($authParams);
     }
 
@@ -74,5 +78,25 @@ class SimpleGoogleLogin {
         $tokenInfoResultJson = $this->http->getRequest($this->URLS['tokenInfo'], $tokenInfoParams, $header);
         $this->tokenInfoResult = json_decode($tokenInfoResultJson->body);
         return $this->tokenInfoResult;
+    }
+
+    /**
+     * fetches all the people, I know
+     */
+    public function getMePeople() {
+        $header = ['authorization: Bearer '. $this->authCodeResult->access_token];
+
+        $tokenInfoResultJson = $this->http->getRequest($this->URLS['mePeople'], [], $header);
+        return json_decode($tokenInfoResultJson->body);
+    }
+
+    /**
+     * fetches the me ressource
+     */
+    public function getMe() {
+        $header = ['authorization: Bearer '. $this->authCodeResult->access_token];
+
+        $tokenInfoResultJson = $this->http->getRequest($this->URLS['me'], [], $header);
+        return json_decode($tokenInfoResultJson->body);
     }
 }
